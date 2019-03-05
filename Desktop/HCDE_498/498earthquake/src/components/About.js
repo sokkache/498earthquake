2465fff792f7aed06f3d2cf77feccb69df41dafa
owlam <owla.mohamed@Outlook.com>
2019-03-04 21:14:07 -0800

    more content

--- Desktop/HCDE_498/498earthquake/src/components/About.js
+++ Desktop/HCDE_498/498earthquake/src/components/About.js
@@ -8,7 +8,11 @@ class About extends Component {
             <section className="About-us">
                 <div className="box">
                 <h1> About Us </h1>
-                <p>We are a group that is dedicated to allowing people in earthquake prone areas to understand the effects and consequences of an earthquake and how to prepare themselves and their family for an earthquake. We help to provide a readiness plan that is customized for each person based on their living situation, as well as scenarios on what to do during earthquakes and general information about how earthquakes occur. 
+                <p>We are a group that is dedicated to allowing people in earthquake prone areas to understand
+                    the effects and consequences of an earthquake and how to prepare themselves and their family
+                     for an earthquake. We help to provide a readiness plan that is customized for each person 
+                     based on their living situation, as well as scenarios on what to do during earthquakes and 
+                     general information about how earthquakes occur. 
                 </p>
                 <Button variant="contained" color="#FF9C27" 
                 //onClick={Info}   onclick brings this to new page
